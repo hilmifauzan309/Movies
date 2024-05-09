694add41630080ed6c361d2e550ccbfe55ec1dfd
hilmifauzan309 <165632588+hilmifauzan309@users.noreply.github.com>
2024-05-09 14:01:45 +0700

    Delete movie directory

--- movie/src/main/java/com/restfull/movie/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.restfull.movie.repository;
-
-import com.restfull.movie.model.MovieModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<MovieModel, Integer> {
-}
